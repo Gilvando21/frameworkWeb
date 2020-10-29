8e60fc76a53cb9e3873682777e9cbe9285379cb3
Gilvando Matos <gilvando0321@gmail.com>
2020-10-28 21:24:24 -0300

    Inclusão do metodo abrirNavegador

--- frameworkTest/src/test/java/framework/pages/Metodos.java
+++ frameworkTest/src/test/java/framework/pages/Metodos.java
@@ -1,5 +1,28 @@
 package framework.pages;
 
-public class Metodos {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class Metodos {
+	WebDriver driver;
+	public void abrirNavegador(String appUrl, String navegador) {
+
+		switch (navegador) {
+		case "CHROME":
+			System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get(appUrl);
+			driver.manage().window().maximize();
+			break;
+
+		case "Firefox":
+			System.setProperty("webdriver.gecko.driver", "C:\\driver\\\\geckodriver.exe.url");
+			driver = new FirefoxDriver();
+			driver.get(appUrl);
+			driver.manage().window().maximize();
+			break;
+	}
+
+}
 }
